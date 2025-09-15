99f904e86f4ad4c2aee8d2178f7445c3def75bef
daosgava <daosgava.garcia@gmail.com>
2025-09-15 18:11:59 -0500

    Add tree DS with classes

---
+++ trees/TreeNode.test.js
@@ -0,0 +1,26 @@
+import assert from "node:assert";
+import test, { describe } from "node:test";
+import { TreeNode } from "./TreeNode.js";
+import { dfs } from "./dfs/dfs.js";
+import { bfs } from "./bfs/bfs.js";
+
+const root = new TreeNode(1);
+
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(7);
+
+describe("TreeNode", () => {
+  test("dfs with TreeNode. It finds number 7", () => {
+    const foundNumber = dfs(root, 7);
+    assert.strictEqual(foundNumber, true);
+  });
+
+  test("bfs with TreeNode. It doesn't find number 8", () => {
+    const foundNumber = bfs(root, 8);
+    assert.strictEqual(foundNumber, false);
+  });
+});
